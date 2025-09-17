fb6ad3553e3a827e7bb06970e0d4d7689b75e9f8
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2025-09-17 16:21:28 +1000

    feat: base tools

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSkip([
+        __DIR__ . '/tests/Fixtures',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withRules([
+        DeclareStrictTypesRector::class,
+    ])
+    ->withPhpSets();
